dff26f2d881ed447065a0c4d547ac68d5257fe6f
Jayaraju Metta <jayarajumetta@gmail.com>
2018-12-09 23:44:22 +0530

    one more commit by MJ

--- javaProgrammes/src/mj/javaProgrammes/FirstGitProgramme.java
+++ javaProgrammes/src/mj/javaProgrammes/FirstGitProgramme.java
@@ -21,8 +21,21 @@ public class FirstGitProgramme {
 		 */
 		
 		// TODO Auto-generated method stub
-		System.out.println("Hello World ");
-		JavaConceptsDocumentation.javaConcepts();
+		//System.out.println("Hello World ");
+		//JavaConceptsDocumentation.javaConcepts();
+		
+		//Instance of a Bicycle -Object Creation
+		BicycleDemo cycle1=new BicycleDemo();
+		cycle1.printState();
+		
+		//Change the Bicycle state and create an instance of it
+		BicycleDemo cycle2=new BicycleDemo();
+		cycle2.changeCadence(5);
+		cycle2.changeGear(6);
+		cycle2.applyBreaks(3);
+		cycle2.speedUp(8);
+		cycle2.printState();
+		
 		
 	}
 
