87b58c2bb105d6f85d0f21b8a81ea745518a380d
Jayaraju Metta <jayarajumetta@gmail.com>
2018-12-09 17:42:11 +0530

    MJ committed Java Programme

---
+++ javaProgrammes/src/mj/javaProgrammes/FirstGitProgramme.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package mj.javaProgrammes;
+
+/**
+ * @author jayarajumetta
+ *
+ */
+public class FirstGitProgramme {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World ");
+
+	}
+
+}
